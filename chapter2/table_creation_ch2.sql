f39305774cd397e23da3ef5460acf4ec9ad98631
Ryan Day <ryandaydev@gmail.com>
2024-04-23 02:45:34 +0000

    Add not null to date

--- chapter2/table_creation_ch2.sql
+++ chapter2/table_creation_ch2.sql
@@ -3,7 +3,7 @@ CREATE TABLE player (
         first_name VARCHAR NOT NULL, 
         last_name VARCHAR NOT NULL, 
         position VARCHAR NOT NULL,
-        last_changed_date DATE, 
+        last_changed_date DATE NOT NULL, 
         PRIMARY KEY (player_id)
 );
 
@@ -12,7 +12,7 @@ CREATE TABLE performance (
         week_number VARCHAR NOT NULL, 
         fantasy_points FLOAT NOT NULL, 
         player_id INTEGER NOT NULL, 
-        last_changed_date DATE,
+        last_changed_date DATE NOT NULL,
         PRIMARY KEY (performance_id), 
         FOREIGN KEY(player_id) REFERENCES player (player_id)
 );
@@ -20,7 +20,7 @@ CREATE TABLE league (
         league_id INTEGER NOT NULL, 
         league_name VARCHAR NOT NULL, 
         scoring_type VARCHAR NOT NULL,
-        last_changed_date DATE,  
+        last_changed_date DATE NOT NULL,  
         PRIMARY KEY (league_id)
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE team (
         team_id INTEGER NOT NULL, 
         team_name VARCHAR NOT NULL, 
         league_id INTEGER NOT NULL, 
-        last_changed_date DATE, 
+        last_changed_date DATE NOT NULL, 
         PRIMARY KEY (team_id), 
         FOREIGN KEY(league_id) REFERENCES league (league_id)
 );
@@ -36,7 +36,7 @@ CREATE TABLE team (
 CREATE TABLE team_player (
         team_id INTEGER NOT NULL, 
         player_id INTEGER NOT NULL, 
-        last_changed_date DATE, 
+        last_changed_date DATE NOT NULL, 
         PRIMARY KEY (team_id, player_id), 
         FOREIGN KEY(team_id) REFERENCES team (team_id), 
         FOREIGN KEY(player_id) REFERENCES player (player_id)
