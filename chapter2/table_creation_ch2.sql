20225eb68f9e6671d250d504686f5eec0b06d414
Ryan Day <ryandaydev@gmail.com>
2024-04-23 02:17:42 +0000

    Added last_change_date and position

--- chapter2/table_creation_ch2.sql
+++ chapter2/table_creation_ch2.sql
@@ -2,6 +2,8 @@ CREATE TABLE player (
         player_id INTEGER NOT NULL, 
         first_name VARCHAR NOT NULL, 
         last_name VARCHAR NOT NULL, 
+        position VARCHAR NOT NULL,
+        last_changed_date DATE, 
         PRIMARY KEY (player_id)
 );
 
@@ -10,13 +12,15 @@ CREATE TABLE performance (
         week_number VARCHAR NOT NULL, 
         fantasy_points FLOAT NOT NULL, 
         player_id INTEGER NOT NULL, 
+        last_changed_date DATE,
         PRIMARY KEY (performance_id), 
         FOREIGN KEY(player_id) REFERENCES player (player_id)
 );
 CREATE TABLE league (
         league_id INTEGER NOT NULL, 
         league_name VARCHAR NOT NULL, 
-        scoring_type VARCHAR NOT NULL, 
+        scoring_type VARCHAR NOT NULL,
+        last_changed_date DATE,  
         PRIMARY KEY (league_id)
 );
 
@@ -24,6 +28,7 @@ CREATE TABLE team (
         team_id INTEGER NOT NULL, 
         team_name VARCHAR NOT NULL, 
         league_id INTEGER NOT NULL, 
+        last_changed_date DATE, 
         PRIMARY KEY (team_id), 
         FOREIGN KEY(league_id) REFERENCES league (league_id)
 );
@@ -31,6 +36,7 @@ CREATE TABLE team (
 CREATE TABLE team_player (
         team_id INTEGER NOT NULL, 
         player_id INTEGER NOT NULL, 
+        last_changed_date DATE, 
         PRIMARY KEY (team_id, player_id), 
         FOREIGN KEY(team_id) REFERENCES team (team_id), 
         FOREIGN KEY(player_id) REFERENCES player (player_id)
