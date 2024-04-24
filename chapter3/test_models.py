ec5c96b4bacde5b07e27a61bd7fe8b9af80b4672
Ryan Day <ryandaydev@gmail.com>
2024-04-24 01:45:39 +0000

    Working pytest by date

--- chapter3/test_models.py
+++ chapter3/test_models.py
@@ -3,6 +3,8 @@ import pytest
 from database import SessionLocal, engine
 import models
 
+from datetime import date, timedelta
+
 @pytest.fixture(scope="function")
 def db_session():
     session = SessionLocal()
@@ -18,6 +20,17 @@ def test_performances_query(db_session):
     performances = db_session.query(models.Performance).all()
     assert len(performances) == 320
     
+def test_performances_date_query(db_session):
+    today = date.today()
+    day_before_yesterday = today - timedelta(days = 2)
+    date_query = db_session.query(models.Performance).filter(
+        models.Performance.last_changed_date >= day_before_yesterday
+    )
+    performances = date_query.all()
+    assert len(performances) == 160
+    
+
+
 def test_leagues_query(db_session):
     leagues = db_session.query(models.League).all()
     assert len(leagues) == 5
@@ -29,3 +42,5 @@ def test_teams_query(db_session):
 def test_team_player_query(db_session):
     team_players = db_session.query(models.TeamPlayer).all()
     assert len(team_players) == 160
+
+
