37022e31d6d0c9b64b8d0cc4f567631ae2bac28d
Ryan Day <ryandaydev@gmail.com>
2024-04-26 12:40:09 +0000

    Backpatch sqlalchemy changes in ch3

--- chapter3/complete/database.py
+++ chapter3/complete/database.py
@@ -1,6 +1,6 @@
 """Database configuration"""
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./fantasy_data.db"
