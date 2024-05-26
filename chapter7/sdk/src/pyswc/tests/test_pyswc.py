6c574782d444bd064dd772decd0505638a175bc4
Ryan Day <ryandaydev@gmail.com>
2024-05-26 21:05:04 +0000

    Black testfile ch7

--- chapter7/sdk/src/pyswc/tests/test_pyswc.py
+++ chapter7/sdk/src/pyswc/tests/test_pyswc.py
@@ -1,13 +1,14 @@
 """Testing the sdk"""
+
 import pytest
 from pyswc import SWC_Client
 
 client = SWC_Client(swc_base_url="http://127.0.0.1:8000")
 
-#analytics endpoints
+
+# analytics endpoints
 def test_health_check():
     """Tests health check from SDK"""
     response = client.get_health_check()
     assert response.status_code == 200
     assert response.json() == {"message": "API health check successful"}
-
