835a763ac4ba9317facaeb1ddeea1f55f07fca4f
Ryan Day <ryandaydev@gmail.com>
2024-05-26 21:03:17 +0000

    Applied black ch7

--- chapter7/sdk/src/pyswc/swc_client.py
+++ chapter7/sdk/src/pyswc/swc_client.py
@@ -1,15 +1,16 @@
-#SDK client
+# SDK client
 import httpx
+
 
 class SWC_Client:
     HEALTH_CHECK_ENDPOINT = "/"
 
     def __init__(self, swc_base_url: str):
-         #initial with config values
+        # initial with config values
         self.swc_base_url = swc_base_url
 
-    #analytics endpoints
+    # analytics endpoints
     def get_health_check(self):
-        #make the API call
+        # make the API call
         with httpx.Client(base_url=self.swc_base_url) as client:
-                        return client.get(self.HEALTH_CHECK_ENDPOINT)
+            return client.get(self.HEALTH_CHECK_ENDPOINT)
