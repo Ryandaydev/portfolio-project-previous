5d5660c7bb0e75dc2898dd5326a9614852894432
Ryan Day <ryandaydev@gmail.com>
2024-05-27 02:27:26 +0000

    Added docstrings to ch7

--- chapter7/sdk/src/pyswc/swc_client.py
+++ chapter7/sdk/src/pyswc/swc_client.py
@@ -1,16 +1,38 @@
-# SDK client
 import httpx
 
 
 class SWC_Client:
+    """Interacts with the Sports World Central API.
+
+        This SDK class simplifies the process of using the SWC fantasy
+        football API. It supports all the functions of SWC API and returns
+        validated datatypes.
+
+    Typical usage example:
+
+        client = SWC_Client()
+        response = client.get_health_check()
+
+    """
+
     HEALTH_CHECK_ENDPOINT = "/"
 
-    def __init__(self, swc_base_url: str):
-        # initial with config values
+    def __init__(self, swc_base_url: str) -> None:
+        """Class constructor that sets the base URL."""
         self.swc_base_url = swc_base_url
 
-    # analytics endpoints
-    def get_health_check(self):
+    def get_health_check(self) -> httpx.Response:
+        """Checks if API is running and healthy.
+
+        Calls the API healtch check endpoint and returns a standard
+        message if the API is running normally. Can be used to check
+        status of API before making more complicated API calls.
+
+        Returns:
+          An httpx.Response object that contains the HTTP status,
+          JSON response and other information received from the API.
+
+        """
         # make the API call
         with httpx.Client(base_url=self.swc_base_url) as client:
             return client.get(self.HEALTH_CHECK_ENDPOINT)
---
+++ chapter7/sdk/src/pyswc/swc_config.py
@@ -0,0 +1,18 @@
+#configuration file
+
+default_url = "https://api.sportsworldcentral.com"
+
+class SWC_Config:
+    swc_base_url: str
+    swc_timeout: int
+    swc_backoff: bool
+    swc_backoff_max_time: int
+
+    def __init__(self, url: str = default_url, timeout: float = 5.0, backoff: bool = True, backoff_max_time: int = 30):
+        self.swc_base_url = url
+        self.swc_timeout = timeout
+        self.swc_backoff = backoff
+        self.swc_backoff_max_time = backoff_max_time
+
+    def __str__(self):
+         return f"{self.swc_base_url} {self.swc_timeout} {self.swc_backoff} {self.swc_backoff_max_time}"
