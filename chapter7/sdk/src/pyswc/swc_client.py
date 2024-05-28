44e897dd142918bcd759fb4b2d8ba32e7e65cc35
Ryan Day <ryandaydev@gmail.com>
2024-05-28 13:09:44 +0000

    Adding bulk endpoints ch7

--- chapter7/sdk/src/pyswc/swc_client.py
+++ chapter7/sdk/src/pyswc/swc_client.py
@@ -24,6 +24,17 @@ class SWCClient:
     HEALTH_CHECK_ENDPOINT = "/"
     GET_LEAGUES_ENDPOINT = "/v0/leagues/"
 
+    BULK_FILE_BASE_URL = ("https://raw.githubusercontent.com/ryandaydev" + 
+                            "/portfolio-project/main/chapter7/sdk/bulk/")
+
+    BULK_FILE_NAMES = {
+        "players": "player_data.csv",
+        "leagues": "league_data.csv",
+        "performances": "performance_data.csv",
+        "teams": "team_data.csv",
+        "team_players": "team_player_data.csv"
+    }
+
     def __init__(self, input_config: config.SWCConfig):
         """Class constructor that sets varibles from configuration object."""
 
@@ -113,5 +124,19 @@ class SWCClient:
         }
 
         endpoint_url = self.build_url(self.GET_LEAGUES_ENDPOINT, params)
+        
         response = self.get_url(endpoint_url)
         return [League(**league) for league in response.json()]
+
+    def get_bulk_players(self) -> bytes:
+        """Returns a CSV file with player data"""
+
+        self.logger.debug("Entered get bulk players")
+
+        player_file_path = (self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["players"])
+        
+        response = httpx.get(player_file_path)
+
+        if response.status_code == 200:
+            self.logger.debug("File downloaded successfully")
+            return response.content
--- chapter7/sdk/tests/test_pyswc.py
+++ chapter7/sdk/tests/test_pyswc.py
@@ -2,6 +2,12 @@ import pytest
 from pyswc import SWCClient
 from pyswc import SWCConfig
 from pyswc.schemas import League
+import csv
+import os
+from io import StringIO
+
+current_dir = os.path.dirname(__file__)
+data_dir = current_dir + "/test_data_output/"
 
 """Unit tests for PYSWC SDK
 
@@ -14,42 +20,70 @@ Typical usage example:
 
 """
 
-def test_health_check():
-    """Tests health check from SDK"""
+# def test_health_check():
+#     """Tests health check from SDK"""
+#     config = SWCConfig()
+#     client = SWCClient(config)
+#     response = client.get_health_check()
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "API health check successful"}
+
+# def test_health_check_with_URL():
+#     """Tests health check from SDK"""
+#     config = SWCConfig(url="http://127.0.0.1:8000")
+#     client = SWCClient(config)    
+#     response = client.get_health_check()
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "API health check successful"}
+
+# def test_get_leagues():
+#     """Tests get leagues from SDK"""
+#     config = SWCConfig(url="http://127.0.0.1:8000")
+#     client = SWCClient(config)    
+#     leagues_response = client.get_leagues()
+#     # Assert the list is not empty
+#     assert isinstance(leagues_response, list)
+#     # Assert each item in the list is an instance of League
+#     for league in leagues_response:
+#         assert isinstance(league, League)
+#     assert len(leagues_response) == 5
+
+# def test_get_leagues_no_backoff():
+#     """Tests get leagues from SDK without backoff"""
+#     config = SWCConfig(url="http://127.0.0.1:8000",backoff=False)
+#     client = SWCClient(config)    
+#     leagues_response = client.get_leagues()
+#     # Assert the list is not empty
+#     assert isinstance(leagues_response, list)
+#     # Assert each item in the list is an instance of League
+#     for league in leagues_response:
+#         assert isinstance(league, League)
+#     assert len(leagues_response) == 5    
+
+#bulk endpoints
+def test_bulk_players():
+
     config = SWCConfig()
-    client = SWCClient(config)
-    response = client.get_health_check()
-    assert response.status_code == 200
-    assert response.json() == {"message": "API health check successful"}
-
-def test_health_check_with_URL():
-    """Tests health check from SDK"""
-    config = SWCConfig(url="http://127.0.0.1:8000")
     client = SWCClient(config)    
-    response = client.get_health_check()
-    assert response.status_code == 200
-    assert response.json() == {"message": "API health check successful"}
 
-def test_get_leagues():
-    """Tests get leagues from SDK"""
-    config = SWCConfig(url="http://127.0.0.1:8000")
-    client = SWCClient(config)    
-    leagues_response = client.get_leagues()
-    # Assert the list is not empty
-    assert isinstance(leagues_response, list)
-    # Assert each item in the list is an instance of League
-    for league in leagues_response:
-        assert isinstance(league, League)
-    assert len(leagues_response) == 5
+    """Tests bulk player download through SDK"""
+    player_file = client.get_bulk_players()
 
-def test_get_leagues_no_backoff():
-    """Tests get leagues from SDK without backoff"""
-    config = SWCConfig(url="http://127.0.0.1:8000",backoff=False)
-    client = SWCClient(config)    
-    leagues_response = client.get_leagues()
-    # Assert the list is not empty
-    assert isinstance(leagues_response, list)
-    # Assert each item in the list is an instance of League
-    for league in leagues_response:
-        assert isinstance(league, League)
-    assert len(leagues_response) == 5    
+
+    # Write the file to disk to verify file download
+    output_file_path = data_dir + 'players_file.csv'
+    with open(output_file_path, 'wb') as f:
+        f.write(player_file)
+
+    # Decode the byte content to a string to test contents
+    player_file_str = player_file.decode('utf-8-sig') 
+    player_file_s = StringIO(player_file_str)
+
+    csv_reader = csv.reader(player_file_s)
+
+    # Assert the file has the correct number of records (including header)
+    rows = list(csv_reader)
+    assert len(rows) == 551
+
+    # Additional check: ensure the first row is the header
+    assert rows[0] == ['player_id','gsis_id','first_name','last_name','position','last_changed_date']
