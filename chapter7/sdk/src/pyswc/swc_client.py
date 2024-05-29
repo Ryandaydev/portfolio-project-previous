6ccfac1e13a34540d32f552690c88f23814dbe49
Ryan Day <ryandaydev@gmail.com>
2024-05-29 01:59:24 +0000

    Added additional endpoints ch7

--- chapter7/sdk/src/pyswc/swc_client.py
+++ chapter7/sdk/src/pyswc/swc_client.py
@@ -2,7 +2,7 @@ import httpx
 import pyswc.swc_config as config
 import logging
 from urllib.parse import urlencode
-from .schemas import League
+from .schemas import League, Team, Player, Performance
 from typing import List
 import backoff
 
@@ -23,16 +23,22 @@ class SWCClient:
 
     HEALTH_CHECK_ENDPOINT = "/"
     LIST_LEAGUES_ENDPOINT = "/v0/leagues/"
+    LIST_PLAYERS_ENDPOINT = "/v0/players/"
+    LIST_PERFORMANCES_ENDPOINT = "/v0/performances/"
+    LIST_TEAMS_ENDPOINT = "/v0/teams/"
+    GET_COUNTS_ENDPOINT = "/v0/counts/"
 
-    BULK_FILE_BASE_URL = ("https://raw.githubusercontent.com/ryandaydev" + 
-                            "/portfolio-project/main/chapter7/sdk/bulk/")
+    BULK_FILE_BASE_URL = (
+        "https://raw.githubusercontent.com/ryandaydev"
+        + "/portfolio-project/main/chapter7/sdk/bulk/"
+    )
 
     BULK_FILE_NAMES = {
         "players": "player_data.csv",
         "leagues": "league_data.csv",
         "performances": "performance_data.csv",
         "teams": "team_data.csv",
-        "team_players": "team_player_data.csv"
+        "team_players": "team_player_data.csv",
     }
 
     def __init__(self, input_config: config.SWCConfig):
@@ -69,6 +75,18 @@ class SWCClient:
             self.logger.error(f"Request error occurred: {str(e)}")
             raise
 
+    def build_url(self, endpoint, params=None) -> str:
+        """Converts dictionary of parameters to query string and appends to URL"""
+        if params:
+            params_dict = {
+                key: value for (key, value) in params.items() if value is not None
+            }
+            query_string = urlencode(params_dict)
+            full_url = endpoint + "?" + query_string
+        else:
+            full_url = endpoint
+        return full_url
+
     def get_health_check(self) -> httpx.Response:
         """Checks if API is running and healthy.
 
@@ -84,18 +102,6 @@ class SWCClient:
         self.logger.debug("Entered health check")
         endpoint_url = self.HEALTH_CHECK_ENDPOINT
         return self.get_url(endpoint_url)
-
-    def build_url(self, endpoint, params=None) -> str:
-        """Converts dictionary of parameters to query string and appends to URL"""
-        if params:
-            params_dict = {
-                key: value for (key, value) in params.items() if value is not None
-            }
-            query_string = urlencode(params_dict)
-            full_url = endpoint + "?" + query_string
-        else:
-            full_url = endpoint
-        return full_url
 
     def list_leagues(
         self,
@@ -114,7 +120,7 @@ class SWCClient:
         SportsWorldCentral fantasy league.
 
         """
-        self.logger.debug("Entered get leagues")
+        self.logger.debug("Entered list leagues")
 
         params = {
             "skip": skip,
@@ -124,18 +130,206 @@ class SWCClient:
         }
 
         endpoint_url = self.build_url(self.LIST_LEAGUES_ENDPOINT, params)
-        
+
         response = self.get_url(endpoint_url)
         return [League(**league) for league in response.json()]
+
+    def get_league_by_id(self, league_id: int) -> League:
+        """Returns a Leagues matching a league_id.
+
+        Calls the API v0/leagues/{league_id} endpoint and returns single League.
+
+        Returns:
+        A schemas.League objects representing one SportsWorldCentral fantasy league.
+
+        """
+        self.logger.debug("Entered get league by ID")
+        # build URL
+        endpoint_url = f"{self.LIST_LEAGUES_ENDPOINT}{league_id}"
+        # make the API call
+        response = self.get_url(endpoint_url)
+        responseLeague = League(**response.json())
+        return responseLeague
+
+    def list_teams(
+        self,
+        skip: int = 0,
+        limit: int = 100,
+        minimum_last_changed_date: str = None,
+        team_name: str = None,
+        league_id: int = None,
+    ):
+        """Returns a List of Teams filtered by parameters.
+
+        Calls the API v0/teams endpoint and returns a list of
+        Team objects.
+
+        Returns:
+        A List of schemas.Team objects. Each represents one
+        team in a SportsWorldCentral fantasy league.
+
+        """
+
+        self.logger.debug("Entered list teams")
+
+        params = {
+            "skip": skip,
+            "limit": limit,
+            "minimum_last_changed_date": minimum_last_changed_date,
+            "team_name": team_name,
+            "league_id": league_id,
+        }
+
+        endpoint_url = self.build_url(self.LIST_TEAMS_ENDPOINT, params)
+
+        response = self.get_url(endpoint_url)
+        return [Team(**team) for team in response.json()]
+
+    def list_players(
+        self,
+        skip: int = 0,
+        limit: int = 100,
+        minimum_last_changed_date: str = None,
+        first_name: str = None,
+        last_name: str = None,
+    ):
+        """Returns a List of Players filtered by parameters.
+
+        Calls the API v0/players endpoint and returns a list of
+        Player objects.
+
+        Returns:
+        A List of schemas.Player objects. Each represents one
+        NFL player.
+
+        """
+        self.logger.debug("Entered list players")
+
+        params = {
+            "skip": skip,
+            "limit": limit,
+            "minimum_last_changed_date": minimum_last_changed_date,
+            "first_name": first_name,
+            "last_name": last_name,
+        }
+
+        endpoint_url = self.build_url(self.LIST_PLAYERS_ENDPOINT, params)
+
+        response = self.get_url(endpoint_url)
+        return [Player(**player) for player in response.json()]
+
+    def get_player_by_id(self, player_id: int):
+        """Returns a Players matching the SWC Player ID.
+
+        Calls the API v0/players/{player_id} endpoint and returns
+        one Player pboject.
+
+        Returns:
+        A List of schemas.Player object representing an
+        NFL player.
+
+        """
+        self.logger.debug("Entered get player by ID")
+
+        # build URL
+        endpoint_url = f"{self.LIST_PLAYERS_ENDPOINT}{player_id}"
+        # make the API call
+        response = self.get_url(endpoint_url)
+        responsePlayer = Player(**response.json())
+        return responsePlayer
+
+    def list_performances(
+        self, skip: int = 0, limit: int = 100, minimum_last_changed_date: str = None
+    ):
+        """Returns a List of Performances filtered by parameters.
+
+        Calls the API v0/performances endpoint and returns a list of
+        Performance objects.
+
+        Returns:
+        A List of schemas.Performance objects. Each represents one
+        player's scoring for one NFL week.
+
+        """
+        self.logger.debug("Entered get performances")
+
+        params = {
+            "skip": skip,
+            "limit": limit,
+            "minimum_last_changed_date": minimum_last_changed_date,
+        }
+
+        endpoint_url = self.build_url(self.LIST_PERFORMANCES_ENDPOINT, params)
+
+        response = self.get_url(endpoint_url)
+        return [Performance(**peformance) for peformance in response.json()]
+
+    # bulk endpoints -------------
 
     def get_bulk_player_file(self) -> bytes:
         """Returns a CSV file with player data"""
 
-        self.logger.debug("Entered get bulk players")
+        self.logger.debug("Entered get bulk player file")
 
-        player_file_path = (self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["players"])
-        
+        player_file_path = self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["players"]
+
         response = httpx.get(player_file_path)
+
+        if response.status_code == 200:
+            self.logger.debug("File downloaded successfully")
+            return response.content
+
+    def get_bulk_league_file(self) -> bytes:
+        """Returns a CSV file with league data"""
+
+        self.logger.debug("Entered get bulk league file")
+
+        league_file_path = self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["leagues"]
+
+        response = httpx.get(league_file_path)
+
+        if response.status_code == 200:
+            self.logger.debug("File downloaded successfully")
+            return response.content
+
+    def get_bulk_performance_file(self) -> bytes:
+        """Returns a CSV file with performance data"""
+
+        self.logger.debug("Entered get bulk performance file")
+
+        performance_file_path = (
+            self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["performances"]
+        )
+
+        response = httpx.get(performance_file_path)
+
+        if response.status_code == 200:
+            self.logger.debug("File downloaded successfully")
+            return response.content
+
+    def get_bulk_team_file(self) -> bytes:
+        """Returns a CSV file with team data"""
+
+        self.logger.debug("Entered get bulk team file")
+
+        team_file_path = self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["teams"]
+
+        response = httpx.get(team_file_path)
+
+        if response.status_code == 200:
+            self.logger.debug("File downloaded successfully")
+            return response.content
+
+    def get_bulk_team_player_file(self) -> bytes:
+        """Returns a CSV file with team player data"""
+
+        self.logger.debug("Entered get bulk team player file")
+
+        team_player_file_path = (
+            self.BULK_FILE_BASE_URL + self.BULK_FILE_NAMES["team_players"]
+        )
+
+        response = httpx.get(team_player_file_path)
 
         if response.status_code == 200:
             self.logger.debug("File downloaded successfully")
--- chapter7/sdk/tests/test_pyswc.py
+++ chapter7/sdk/tests/test_pyswc.py
@@ -1,7 +1,7 @@
 import pytest
 from pyswc import SWCClient
 from pyswc import SWCConfig
-from pyswc.schemas import League
+from pyswc.schemas import League, Team, Player, Performance
 import csv
 import os
 from io import StringIO
@@ -19,6 +19,10 @@ Typical usage example:
     pytest test_pyswc.py
 
 """
+
+
+config = SWCConfig(url="http://127.0.0.1:8000",backoff=False)
+client = SWCClient(config)    
 
 # def test_health_check():
 #     """Tests health check from SDK"""
@@ -60,13 +64,112 @@ def test_list_leagues_no_backoff():
         assert isinstance(league, League)
     assert len(leagues_response) == 5    
 
+def test_get_leagues_with_filter():
+    """Tests get leagues from SDK"""
+    
+    leagues_response = client.list_leagues(league_name='Pigskin Prodigal Fantasy League')
+
+    # Assert the list is not empty
+    assert isinstance(leagues_response, list)
+    # Assert each item in the list is an instance of League
+    for league in leagues_response:
+        assert isinstance(league, League)
+    assert len(leagues_response) == 1
+
+
+def test_get_league_by_id():
+    """Tests get leagues from SDK"""
+
+    league_response = client.get_league_by_id(5002)
+        
+    assert isinstance(league_response, League)
+    assert len(league_response.teams) == 8        
+
+def test_list_teams():
+    """Tests list teams from SDK"""
+    
+    teams_response = client.list_teams()
+
+    # Assert the list is not empty
+    assert isinstance(teams_response, list)
+    # Assert each item in the list is an instance of League
+    for team in teams_response:
+        assert isinstance(team, Team)
+    assert len(teams_response) == 20
+
+
+
+#players
+def test_list_players():
+    """Tests get players from SDK"""
+    
+    players_response = client.list_players(skip=0,limit=600)
+
+    # Assert the list is not empty
+    assert isinstance(players_response, list)
+    # Assert each item in the list is an instance of League
+    for player in players_response:
+        assert isinstance(player, Player)
+    assert len(players_response) == 550
+
+
+def test_list_players_by_name():
+    """Tests that the count of players in the database is what is expected"""
+    players_response = client.list_players(first_name="Bryce", last_name="Young")
+
+    # Assert the list is not empty
+    assert isinstance(players_response, list)
+    # Assert each item in the list is an instance of League
+    for player in players_response:
+        assert isinstance(player, Player)
+    assert len(players_response) == 1
+    assert players_response[0].player_id == 102
+
+
+def test_get_player_by_id():
+    """Tests get player by ID from SDK"""
+
+    player_response = client.get_player_by_id(102)
+
+    assert isinstance(player_response, Player)
+    assert player_response.first_name == "Bryce"       
+
+#scoring endpoints
+def test_list_performances():
+    """Tests get peformances from SDK"""
+
+    performances_response = client.list_performances(skip=0,limit=2000)
+
+
+    # Assert the list is not empty
+    assert isinstance(performances_response, list)
+    # Assert each item in the list is an instance of League
+    for performance in performances_response:
+        assert isinstance(performance, Performance)
+    assert len(performances_response) == 1100
+
+
+#test /v0/performances/ with changed date
+def test_list_performances_by_date():
+    """Tests get peformances from SDK"""
+    
+    performances_response = client.list_performances(skip=0,limit=2000,minimum_last_changed_date="2024-04-01")
+
+    # Assert the list is not empty
+    assert isinstance(performances_response, list)
+    # Assert each item in the list is an instance of League
+    for performance in performances_response:
+        assert isinstance(performance, Performance)
+    assert len(performances_response) == 550
+
+
 #bulk endpoints
 def test_bulk_player_file():
+    """Tests bulk player download through SDK"""
 
     config = SWCConfig()
     client = SWCClient(config)    
 
-    """Tests bulk player download through SDK"""
     player_file = client.get_bulk_player_file()
 
 
@@ -87,3 +190,107 @@ def test_bulk_player_file():
 
     # Additional check: ensure the first row is the header
     assert rows[0] == ['player_id','gsis_id','first_name','last_name','position','last_changed_date']
+
+def test_bulk_league_file():
+    """Tests bulk league download through SDK"""
+
+    config = SWCConfig()
+    client = SWCClient(config)    
+
+    league_file = client.get_bulk_league_file()
+
+    # Write the file to disk to verify file download
+    output_file_path = data_dir + 'leagues_file.csv'
+    with open(output_file_path, 'wb') as f:
+        f.write(league_file)
+
+    # Decode the byte content to a string to test contents
+    league_file_str = league_file.decode('utf-8-sig') 
+    league_file_s = StringIO(league_file_str)
+
+    csv_reader = csv.reader(league_file_s)
+
+    # Assert the file has the correct number of records (including header)
+    rows = list(csv_reader)
+    assert len(rows) == 6
+
+    # Additional check: ensure the first row is the header
+    assert rows[0] == ['league_id','league_name','scoring_type','last_change_date']
+
+def test_bulk_performance_file():
+    """Tests bulk performance download through SDK"""
+
+    config = SWCConfig()
+    client = SWCClient(config)    
+
+    performance_file = client.get_bulk_performance_file()
+
+    # Write the file to disk to verify file download
+    output_file_path = data_dir + 'performances_file.csv'
+    with open(output_file_path, 'wb') as f:
+        f.write(performance_file)
+
+    # Decode the byte content to a string to test contents
+    performance_file_str = performance_file.decode('utf-8-sig') 
+    performance_file_s = StringIO(performance_file_str)
+
+    csv_reader = csv.reader(performance_file_s)
+
+    # Assert the file has the correct number of records (including header)
+    rows = list(csv_reader)
+    assert len(rows) == 1101
+
+    # Additional check: ensure the first row is the header
+    assert rows[0] == ['performance_id','week_number','fantasy_points','player_id','last_change_date']
+
+def test_bulk_team_file():
+    """Tests bulk team download through SDK"""
+
+    config = SWCConfig()
+    client = SWCClient(config)    
+
+    team_file = client.get_bulk_team_file()
+
+    # Write the file to disk to verify file download
+    output_file_path = data_dir + 'teams_file.csv'
+    with open(output_file_path, 'wb') as f:
+        f.write(team_file)
+
+    # Decode the byte content to a string to test contents
+    team_file_str = team_file.decode('utf-8-sig') 
+    team_file_s = StringIO(team_file_str)
+
+    csv_reader = csv.reader(team_file_s)
+
+    # Assert the file has the correct number of records (including header)
+    rows = list(csv_reader)
+    assert len(rows) == 21
+
+    # Additional check: ensure the first row is the header
+    assert rows[0] == ['team_id','team_name','league_id','last_change_date']
+
+def test_bulk_team_player_file():
+    """Tests bulk team_player download through SDK"""
+
+    config = SWCConfig()
+    client = SWCClient(config)    
+
+    team_player_file = client.get_bulk_team_player_file()
+    
+    # Write the file to disk to verify file download
+    output_file_path = data_dir + 'team_player_file.csv'
+    with open(output_file_path, 'wb') as f:
+        f.write(team_player_file)
+
+    # Decode the byte content to a string to test contents
+    team_player_file_str = team_player_file.decode('utf-8-sig') 
+    team_player_file_s = StringIO(team_player_file_str)
+
+    csv_reader = csv.reader(team_player_file_s)
+
+    # Assert the file has the correct number of records (including header)
+    rows = list(csv_reader)
+    assert len(rows) == 141
+
+    # Additional check: ensure the first row is the header
+    assert rows[0] == ['team_id','player_id','last_change_date']
