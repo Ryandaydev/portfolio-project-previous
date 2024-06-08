19040e262e98e43ad3607c7f70ca38682cea6b63
Ryan Day <ryandaydev@gmail.com>
2024-06-08 22:28:33 +0000

    Add league_id

--- chapter4/complete/test_main.py
+++ chapter4/complete/test_main.py
@@ -3,17 +3,20 @@ from main import app
 
 client = TestClient(app)
 
-#test the health check endpoint
+
+# test the health check endpoint
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "API health check successful"}
 
-#test /v0/players/
+
+# test /v0/players/
 def test_read_players():
     response = client.get("/v0/players/?skip=0&limit=10000")
     assert response.status_code == 200
     assert len(response.json()) == 550
+
 
 def test_read_players_by_name():
     response = client.get("/v0/players/?first_name=Bryce&last_name=Young")
@@ -21,57 +24,63 @@ def test_read_players_by_name():
     assert len(response.json()) == 1
     assert response.json()[0].get("player_id") == 102
 
-#test /v0/players/{player_id}/
+
+# test /v0/players/{player_id}/
 def test_read_players_with_id():
     response = client.get("/v0/players/101/")
     assert response.status_code == 200
     assert response.json().get("player_id") == 101
 
-#test /v0/performances/
+
+# test /v0/performances/
 def test_read_performances():
     response = client.get("/v0/performances/?skip=0&limit=10000")
     assert response.status_code == 200
     assert len(response.json()) == 1100
 
-#test /v0/performances/ with changed date
+
+# test /v0/performances/ with changed date
 def test_read_performances_by_date():
-    response = client.get("/v0/performances/?skip=0&limit=10000&minimum_last_changed_date=2024-04-01")
+    response = client.get(
+        "/v0/performances/?skip=0&limit=10000&minimum_last_changed_date=2024-04-01"
+    )
     assert response.status_code == 200
     assert len(response.json()) == 550
 
-#test /v0/leagues/{league_id}/
+
+# test /v0/leagues/{league_id}/
 def test_read_leagues_with_id():
     response = client.get("/v0/leagues/5002/")
     assert response.status_code == 200
-    assert len(response.json()['teams']) == 8
+    assert len(response.json()["teams"]) == 8
 
-#test /v0/leagues/
+
+# test /v0/leagues/
 def test_read_leagues():
     response = client.get("/v0/leagues/?skip=0&limit=500")
     assert response.status_code == 200
     assert len(response.json()) == 5
 
-#test /v0/teams/
+
+# test /v0/teams/
 def test_read_teams():
     response = client.get("/v0/teams/?skip=0&limit=500")
     assert response.status_code == 200
     assert len(response.json()) == 20
 
-#test /v0/teams/
+
+# test /v0/teams/
 def test_read_teams_for_one_league():
     response = client.get("/v0/teams/?skip=0&limit=500&league_id=5001")
     assert response.status_code == 200
     assert len(response.json()) == 12
 
 
-
-#test the count functions
+# test the count functions
 def test_counts():
     response = client.get("/v0/counts/")
     response_data = response.json()
-    assert response.status_code == 200    
-    assert response_data['league_count'] == 5
-    assert response_data['team_count'] == 20
-    assert response_data['player_count'] == 550
-
-
+    assert response.status_code == 200
+    assert response_data["league_count"] == 5
+    assert response_data["team_count"] == 20
+    assert response_data["player_count"] == 550
--- chapter5/complete/main.py
+++ chapter5/complete/main.py
@@ -1,4 +1,5 @@
 """FastAPI program - Chapter 5"""
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from datetime import date
@@ -8,6 +9,7 @@ from database import SessionLocal
 
 app = FastAPI()
 
+
 # Dependency
 def get_db():
     db = SessionLocal()
@@ -16,14 +18,29 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/")
 async def root():
     return {"message": "API health check successful"}
 
 
 @app.get("/v0/players/", response_model=list[schemas.Player])
-def read_players(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None, first_name: str = None, last_name: str = None,db: Session = Depends(get_db)):
-    players = crud.get_players(db, skip=skip, limit=limit, min_last_changed_date=minimum_last_changed_date, first_name=first_name, last_name=last_name)
+def read_players(
+    skip: int = 0,
+    limit: int = 100,
+    minimum_last_changed_date: date = None,
+    first_name: str = None,
+    last_name: str = None,
+    db: Session = Depends(get_db),
+):
+    players = crud.get_players(
+        db,
+        skip=skip,
+        limit=limit,
+        min_last_changed_date=minimum_last_changed_date,
+        first_name=first_name,
+        last_name=last_name,
+    )
     return players
 
 
@@ -34,34 +51,71 @@ def read_player(player_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Player not found")
     return player
 
+
 @app.get("/v0/performances/", response_model=list[schemas.Performance])
-def read_performances(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None, db: Session = Depends(get_db)):
-    performances = crud.get_performances(db, skip=skip, limit=limit, min_last_changed_date=minimum_last_changed_date)
+def read_performances(
+    skip: int = 0,
+    limit: int = 100,
+    minimum_last_changed_date: date = None,
+    db: Session = Depends(get_db),
+):
+    performances = crud.get_performances(
+        db, skip=skip, limit=limit, min_last_changed_date=minimum_last_changed_date
+    )
     return performances
 
+
 @app.get("/v0/leagues/{league_id}", response_model=schemas.League)
-def read_league(league_id: int,db: Session = Depends(get_db)):
-    league = crud.get_league(db, league_id = league_id)
+def read_league(league_id: int, db: Session = Depends(get_db)):
+    league = crud.get_league(db, league_id=league_id)
     if league is None:
         raise HTTPException(status_code=404, detail="League not found")
     return league
 
 
 @app.get("/v0/leagues/", response_model=list[schemas.League])
-def read_leagues(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None, league_name: str = None,db: Session = Depends(get_db)):
-    leagues = crud.get_leagues(db, skip=skip, limit=limit, min_last_changed_date=minimum_last_changed_date, league_name=league_name)
+def read_leagues(
+    skip: int = 0,
+    limit: int = 100,
+    minimum_last_changed_date: date = None,
+    league_name: str = None,
+    db: Session = Depends(get_db),
+):
+    leagues = crud.get_leagues(
+        db,
+        skip=skip,
+        limit=limit,
+        min_last_changed_date=minimum_last_changed_date,
+        league_name=league_name,
+    )
     return leagues
 
+
 @app.get("/v0/teams/", response_model=list[schemas.Team])
-def read_teams(skip: int = 0, limit: int = 100, minimum_last_changed_date: date = None, team_name: str = None, league_id: int = None, db: Session = Depends(get_db)):
-    teams = crud.get_teams(db, skip=skip, limit=limit, min_last_changed_date=minimum_last_changed_date, team_name=team_name, league_id=league_id)
+def read_teams(
+    skip: int = 0,
+    limit: int = 100,
+    minimum_last_changed_date: date = None,
+    team_name: str = None,
+    league_id: int = None,
+    db: Session = Depends(get_db),
+):
+    teams = crud.get_teams(
+        db,
+        skip=skip,
+        limit=limit,
+        min_last_changed_date=minimum_last_changed_date,
+        team_name=team_name,
+        league_id=league_id,
+    )
     return teams
 
 
 @app.get("/v0/counts/", response_model=schemas.Counts)
 def get_count(db: Session = Depends(get_db)):
     counts = schemas.Counts(
-        league_count = crud.get_league_count(db),
-        team_count = crud.get_team_count(db),
-        player_count = crud.get_player_count(db))
+        league_count=crud.get_league_count(db),
+        team_count=crud.get_team_count(db),
+        player_count=crud.get_player_count(db),
+    )
     return counts
